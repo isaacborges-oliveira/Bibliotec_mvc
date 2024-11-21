b1a01066f96a074482ff7c82e0f06a28f3be25c8
ISAAC BORGES OLIVEIRA <43022067801@info.local>
2024-11-21 16:53:44 -0300

    css html bibliotec

--- Models/LivroCategoria.cs
+++ Models/LivroCategoria.cs
@@ -4,16 +4,17 @@ namespace Bibliotec.Models
 {
     public class LivroCategoria
     {
-    }
-        public int LivroCategoriaID {get; set;}
+
+        public int LivroCategoriaID { get; set; }
 
         [ForeignKey("Livro")]
-        public int LivroID {get; set;}
+        public int LivroID { get; set; }
 
-        public Livro Livro {get; set;}
+        public Livro Livro { get; set; }
 
-         [ForeignKey("Categoria")]
-        public int CategoriaID {get; set;}
+        [ForeignKey("Categoria")]
+        public int CategoriaID { get; set; }
 
-        public Categoria Categoria {get; set;}
+        public Categoria Categoria { get; set; }
+    }
 }
--- Models/LivroReserva.cs
+++ Models/LivroReserva.cs
@@ -3,6 +3,6 @@ namespace Bibliotec.Models
     public class LivroReserva
     {
         public int LivroReservaID {get; set;}
-        public 
+    
     }
 }
